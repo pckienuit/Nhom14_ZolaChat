e28d98d9c2e4b82e6651f658c0133e28c02b9daf
Phan Chi Kien <23520804@gm.uit.edu.vn>
2025-12-11 10:22:40 +0700

    feat: Introduce Gradle Version Catalogs for dependency management and configure the main application module with Android and Firebase dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.doan_zaloclone"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.doan_zaloclone"
